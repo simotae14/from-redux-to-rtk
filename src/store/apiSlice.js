a9e48b1ac72cb594234a2109193a5cffbc9cc204
Simone Taeggi <startae14@gmail.com>
2023-11-15 23:57:32 +0100

    Migrate a Redux POST request in RTK Query Mutation

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -9,7 +9,18 @@ export const api = createApi({
     getService: builder.query({
       query: (serviceId) => `/services/${serviceId}`,
     }),
+    makeContact: builder.mutation({
+      query: (body) => ({
+        url: '/contact',
+        method: 'POST',
+        body,
+      }),
+    }),
   }),
 });
 
-export const { useGetServicesQuery, useGetServiceQuery } = api;
+export const { 
+  useGetServicesQuery,
+  useGetServiceQuery,
+  useMakeContactMutation
+} = api;
