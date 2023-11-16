c3fee17a851454062ef3f481dd1d4e40ff0b4c6e
Simone Taeggi <startae14@gmail.com>
2023-11-16 11:03:12 +0100

    Prefetch Data in RTK Query using initiate method

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { api } from "./store/apiSlice";
 import App from "./App";
 import "./index.css";
 
@@ -12,6 +13,10 @@ if (import.meta.env.DEV) {
     onUnhandledRequest: "bypass",
   });
 }
+
+// prefetch data
+store.dispatch(api.endpoints.getServices.initiate());
+store.dispatch(api.endpoints.getDogs.initiate());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
