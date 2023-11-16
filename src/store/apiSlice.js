47a93a3c5fd037097c518bc1c2684f54c2f70128
Simone Taeggi <startae14@gmail.com>
2023-11-16 10:29:33 +0100

    Invalidate RTK Query Cache using keepUnusedDataFor for the entire slice

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const api = createApi({
+  keepUnusedDataFor: 5, // in seconds
   baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
   endpoints: (builder) => ({
     getServices: builder.query({
